a062d991f241599d19d1f36ac3937ec997c1d80d
queni <queni@queni-PC.mshome.net>
2016-05-30 14:29:33 +0200

    working, auto search checkbox added

--- index.php
+++ index.php
@@ -25,25 +25,70 @@ and open the template in the editor.
     <body>
         <?php
         ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center" id="displayDiv">
-                    <h1>Wikipedia Viewer</h1>
-                    <form>
-                        <!--<div class="form-group">-->
-<!--                            <label>
-                                Search
-                            </label>-->
-                            <input type="text" class="form-control" id="searchInput" placeholder="Search Here">
-                        <!--</div>-->
-                    </form>
-                    <p id="displayP"></p>
-                    <div id="resultDiv">
-                        
+        <main>
+            <div class="container">
+                <div class="row">
+                    <!--<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center" id="displayDiv">-->
+                    <!--<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 text-center" id="displayDiv">-->
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center" id="displayDiv">
+                        <h1>Wikipedia Viewer</h1>
+                        <form>
+                            <div class="form-group">
+
+                                <!--                                <label>
+                                                                    <h3>Search here </h3>
+                                                                </label>-->
+                                <!--                                <div class="row">
+                                                                    </div>
+                                
+                                                                <input type="text" class="form-control" id="searchInput" placeholder="Search here">
+                                                                <input type="text" class="form-control col-xs-12 col-sm-6 col-sm-offset-3" id="searchInput" placeholder="Search here">
+                                                                    <input type="text" class="col-xs-12 col-sm-6" id="searchInput" placeholder="Search here">
+                                                 s                     </div>
+                                                                    <a href="https://en.wikipedia.org/wiki/Special:Random" class="btn btn-default"> Random </a>
+                                                                    <div class="row">
+                                                                    <a href="https://en.wikipedia.org/wiki/Special:Random" class="btn btn-default col-xs-12 col-sm-6 col-sm-offset-3"> 
+                                                                    <a href="https://en.wikipedia.org/wiki/Special:Random" class="btn btn-default col-xs-12 col-sm-6"> 
+                                                                        Random page
+                                                                    </a>
+                                                                </div>-->
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <div class="input-group">
+                                            <input type="text" id="searchInput" class="form-control" placeholder="Search here">
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-default" id="searchBtn">Search</button>
+                                                <a href="https://en.wikipedia.org/wiki/Special:Random" class="btn btn-default"> 
+                                                    Random page
+                                                </a>
+                                            </span>  
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12">
+                                        <label>
+                                            <!--<input type="checkbox" id="autoSearch" class="" checked="checked">-->
+                                            <input checked type="checkbox" id="autoSearch" name="autoSearch" class="" checked/>
+<!--                                            <input type="checkbox" name="df" checked>
+                                            <input type="checkbox" checked="checked">
+                                            <input type="checkbox" checked>
+                                            <input type="checkbox">-->
+                                            Automatic Search 
+                                        </label>  
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+                        <p id="displayP"></p>
+                        <div id="resultDiv">
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+        </main>
+        <footer class="text-center">
+            Coded by <a href="https://github.com/QGfcc">QG</a>
+        </footer>
 
     </body>
 </html>
--- js/wikipediaViewer.js
+++ js/wikipediaViewer.js
@@ -4,32 +4,84 @@
  * and open the template in the editor.
  */
 
-
+function displayLoading() {
+    $("#displayP").text("Loading...");
+    $("#resultDiv *").css("color", "lightgrey");
+//    $("#resultDiv * ").css("opacity","0.4");
+}
+function unDisplayLoading() {
+    $("#displayP").text("");
+}
+function displayNoResult() {
+    $("#displayP").text("No result available for your search.");
+}
+function collapseToggle(id) {
+    var body = "#panelBody" + id;
+    var readMore = "#readMore" + id;
+//        $(".collapsed").removeClass("collapsed");
+//        e.target.nodeName.addClass("targeted");
+//    alert("test");
+//    alert(e.target);
+//    alert(e.target.nodeName);
+//    alert(id);
+//        e.target.text("targeted");
+//    e.target.innerHTML = "targeted";
+//    e.target.addClass("targeted");
+//    $(".targeted").css("background-color", "red !important");
+//    $("#"+id+" div").css("background-color", "red !important");
+//    $("#"+id+" div").css("overflow", "auto");
+//    $("#"+id+" div").css("max-height", "2000px");
+//    $("#"+id).css("max-height", "2000px");
+    if ($(body).hasClass("collapsed")) {
+        $(body).removeClass("collapsed");
+        $(readMore).text("Read less");
+    } else {
+        $(body).addClass("collapsed");
+        $(readMore).text("Read more");
+    }
+//    $("#"+id+" div").text("dqfdqf");
+//    alert("test");
+//        $(".collapsed").css("max-height","inherit");
+//        $(".panel-body").css("max-height","inherit");
+}
 function displayData(JSONObj) {
     $("#resultDiv").text("");
-    for (var key in JSONObj.query.pages) {
+    var i = 0;
+    if (JSONObj.hasOwnProperty("query") && JSONObj.query.hasOwnProperty("pages")) {
+        for (var key in JSONObj.query.pages) {
 //        if (Object.prototype.hasOwnProperty.call(JSONObj, key)) {
 //        if (JSONObj.hasOwnProperty(key)) {
-        var title = $("<div class=''></div>").text(JSONObj.query.pages[key].title);
-        var summary = $("<div class=''></div>").text(JSONObj.query.pages[key].extract);
+            var title = $("<div class=''></div>").text(JSONObj.query.pages[key].title);
+            var summary = $("<div class=''></div>").text(JSONObj.query.pages[key].extract);
 //        var aTag = $("<a class='aBlock'></a>").append(title).append(summary);
-        var aTag1 = $("<a class='aBlock panel-heading'></a>").append(title);
+            var aTag1 = $("<a class='aBlock panel-heading'></a>").append(title);
 //        var aTag2 = $("<a class='aBlock panel-body'></a>").append(summary);
-        var aTag2 = $("<a class='aBlock panel-body collapsed'></a>").append(summary);
+            var aTag2 = $("<a class='aBlock panel-body collapsed'></a>").append(summary);
 //        var title = $("<div class='panel-heading'></div>").text(JSONObj.query.pages[key].title)
 //        var summary = $("<div class='panel-body'></div>").text(JSONObj.query.pages[key].extract);
 //        var aTag = $("<a class='aBlock panel-heading'></a>").append(title).append(summary);
-        var url = JSONObj.query.pages[key].canonicalurl;
-        aTag1.attr("href", url);
-        aTag2.attr("href", url);
-        var readMoreDiv = $("<div class='panel-footer readMore' id='readMore'></div>").text("Read more");
-        var container = $("<div></div>").append(aTag1).append(aTag2).append(readMoreDiv);
+            var url = JSONObj.query.pages[key].canonicalurl;
+            aTag1.attr("href", url);
+            aTag2.attr("href", url);
+            aTag2.attr("id", "panelBody" + i);
+            var readMoreDiv = $("<div class='panel-footer readMore' id='readMore" + i + "'></div>").text("Read more");
+            readMoreDiv.click({id: i}, function (e) {
+                collapseToggle(e.data.id);
+            });
+            var container = $("<div></div>").append(aTag1).append(aTag2).append(readMoreDiv);
 //        container.attr("class", "displayContainer thumbnail text-center panel panel-default");
-        container.attr("class", "displayContainer text-center panel panel-info");
-        $("#resultDiv").append(container);
-
+            container.attr("class", "displayContainer text-center panel panel-info");
+            $("#resultDiv").append(container);
+//        $("#panelBody"+i).click({id: "panelBody"+i}, function (e) {
+//            readMore(e.data.id);
+//        });
+            i++;
+        }
+        unDisplayLoading();
+    } else {
+        displayNoResult();
     }
-    unDisplayLoading();
+//    $(".panel-footer").on("click", function (e) {
 }
 
 function searchWikiJSONP(query, numHit) {
@@ -66,13 +118,6 @@ function searchWikiJSONP(query, numHit) {
         }
     });
 }
-function displayLoading() {
-    $("#displayP").text("Loading...");
-    $("resultDiv").css("color", "lightgrey");
-}
-function unDisplayLoading() {
-    $("#displayP").text("");
-}
 function search() {
     inputVal = $("#searchInput").val();
     if (inputVal != "") {
@@ -80,6 +125,7 @@ function search() {
         searchWikiJSONP(inputVal, 20);
     }
 }
+var isAutoSearch = 1;
 $(document).ready(function () {
     $("#searchInput").keypress(function (event) {
         if (event.keyCode == 13) {
@@ -89,17 +135,26 @@ $(document).ready(function () {
     $("#searchInput").keyup(function (event) {
         if (event.keyCode == 13) {
             event.preventDefault();
-        } else {
+            if (isAutoSearch === 0) {
+                search();
+            }
+        } else if (isAutoSearch === 1) {
             search();
         }
     });
-//alert('test');
-//    $("div.readMore").click(function () {
-    $(".panel-heading").click(function () {
-//        $(".collapsed").removeClass("collapsed");
-//        $(".collapsed").css("max-height","inherit");
-        alert('test');
-//        $(".panel-body").css("max-height","inherit");
+    $("#searchBtn").click(function (event) {
+        event.preventDefault();
+        search();
+    });
+//    $("#autoSearch").attr("checked","checked");
+    $("#autoSearch").prop( "checked", true );
+    $("#autoSearch").click(function () {
+        if ($(this).is(':checked')) {
+            isAutoSearch = 1;
+            search();
+        } else {
+            isAutoSearch = 0;
+        }
     });
     search();
 });
